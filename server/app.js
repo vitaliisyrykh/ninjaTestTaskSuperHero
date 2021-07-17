2858ddcd7c9cabc1906ce911f883a3ef88041206
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-17 16:26:56 +0300

    Add main router

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
+const router = require('./router/index')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', router)
 
 module.exports=app;
