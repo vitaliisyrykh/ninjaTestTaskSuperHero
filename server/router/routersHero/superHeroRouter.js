9076759524ffba57af4f153fb2e111865d38b70e
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-18 20:11:04 +0300

    Add imgsRouter

--- server/router/routersHero/superHeroRouter.js
+++ server/router/routersHero/superHeroRouter.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 
 const pagination = require('../../middlewares/pagination.mw');
 const superHeroController = require('../../controllers/superHeroController');
-const {checkHero} = require('../../middlewares/checkHero.mw')
+const { checkHero } = require('../../middlewares/checkHero.mw');
 const superPowerRouter = require('./superPowerRouter');
-
+const imgsRouter = require('./imgHeroRouter');
+ 
 
 const superHeroRouter = Router({
   mergeParams: true
@@ -16,10 +17,11 @@ superHeroRouter
 
 superHeroRouter
   .route('/:idHero')
-  .get(checkHero,superHeroController.getHero)
+  .get(checkHero, superHeroController.getHero)
   .patch(superHeroController.updateHero)
   .delete(superHeroController.deleteHero);
 
-superHeroRouter.use('/:idHero/superpower', superPowerRouter)  
+superHeroRouter.use('/:idHero/superpower', superPowerRouter);
+superHeroRouter.use('/:idHero/imgs', imgsRouter)
 
 module.exports = superHeroRouter;
