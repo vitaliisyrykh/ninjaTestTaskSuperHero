a85eb0dec050afd998b3cd689927d83c8161724f
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-19 12:25:24 +0300

    Add uploadMW to post add imgRouter

--- server/router/routersHero/superHeroRouter.js
+++ server/router/routersHero/superHeroRouter.js
@@ -5,14 +5,12 @@ const superHeroController = require('../../controllers/superHeroController');
 const { checkHero } = require('../../middlewares/checkHero.mw');
 const superPowerRouter = require('./superPowerRouter');
 const imgsRouter = require('./imgHeroRouter');
- 
+const uploadImgs = require('../../middlewares/uploadImgs');
 
-const superHeroRouter = Router({
-  mergeParams: true
-});
+const superHeroRouter = Router();
 superHeroRouter
   .route('/')
-  .post(superHeroController.createSuperHero)
+  .post(uploadImgs, superHeroController.createSuperHero)
   .get(pagination, superHeroController.getAllHeroes);
 
 superHeroRouter
@@ -22,6 +20,6 @@ superHeroRouter
   .delete(superHeroController.deleteHero);
 
 superHeroRouter.use('/:idHero/superpower', superPowerRouter);
-superHeroRouter.use('/:idHero/imgs', imgsRouter)
+superHeroRouter.use('/:idHero/imgs', imgsRouter);
 
 module.exports = superHeroRouter;
