4d5d41d3cc26a3cc2a5f6c86e38b02e1156d1bcf
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-21 15:06:39 +0300

    Add deleteHandler

--- front/front/src/components/Hero.js
+++ front/front/src/components/Hero.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { useDispatch } from 'react-redux';
+import {deleteHeroRequest} from '../action/creatorHeroAction';
 
 const Hero = props => {
-  const{hero:{nickName,realName,discription,chartPhrase}}=props;
+  const {
+    hero: { nickName, realName, discription, chartPhrase, id }
+  } = props;
+
+  const dispatch = useDispatch();
+
+  const deleteHandler = ()=>{
+    dispatch(deleteHeroRequest({id}))
+  }
+  
 
   return (
     <>
@@ -11,9 +21,9 @@ const Hero = props => {
       <p>{discription}</p>
       <span>{chartPhrase}</span>
       <button>update</button>
-      <button>delete</button>
+      <button onClick={deleteHandler}>delete</button>
     </>
   );
-}
+};
 
 export default Hero;
