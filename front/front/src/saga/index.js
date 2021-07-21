9ff72321faa27ec5ef39d363527258851aaad529
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-21 14:13:58 +0300

    Add getHeroes

--- front/front/src/saga/index.js
+++ front/front/src/saga/index.js
@@ -1,9 +1,10 @@
 import { takeLatest } from '@redux-saga/core/effects';
 import ACTION_TYPES from '../action/actionTypes';
-import {createHero} from './heroSaga';
+import {createHero, getHeroes} from './heroSaga';
 
 function * rootSaga(){
   yield takeLatest(ACTION_TYPES.CREATE_HERO_REQUEST, createHero);
+  yield takeLatest(ACTION_TYPES.GET_HEROES_REQUEST, getHeroes);
 }
 
 export default rootSaga;
