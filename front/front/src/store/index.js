8f120f4b19a9335d89c1cf8ddf1d60945afd29ac
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-21 14:13:43 +0300

    Add devToolsRedux

--- front/front/src/store/index.js
+++ front/front/src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducer/index';
 import rootSaga from '../saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 export default store;
