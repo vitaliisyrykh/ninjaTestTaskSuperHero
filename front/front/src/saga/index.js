2ff4ab9fe87daf029d61d84fd8b9758a3987c9c6
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-21 15:05:22 +0300

    Add deleteHero

--- front/front/src/saga/index.js
+++ front/front/src/saga/index.js
@@ -1,10 +1,11 @@
 import { takeLatest } from '@redux-saga/core/effects';
 import ACTION_TYPES from '../action/actionTypes';
-import {createHero, getHeroes} from './heroSaga';
+import {createHero, getHeroes, deleteHero} from './heroSaga';
 
 function * rootSaga(){
   yield takeLatest(ACTION_TYPES.CREATE_HERO_REQUEST, createHero);
   yield takeLatest(ACTION_TYPES.GET_HEROES_REQUEST, getHeroes);
+  yield takeLatest(ACTION_TYPES.DELETE_HERO_REQUEST, deleteHero);
 }
 
 export default rootSaga;
