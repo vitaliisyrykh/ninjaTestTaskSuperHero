dab0e8109008ad99ad8481410576dc035e707ae0
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-21 14:13:22 +0300

    Add formatCode

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const router = require('./router/index')
-const app = express();
 
 app.use(cors());
 app.use(express.json());
