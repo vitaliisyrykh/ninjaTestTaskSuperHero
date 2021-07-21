12fc2cb8c52ef5d546602e355f7abca604918f6b
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-21 14:17:38 +0300

    Add getheroes request,success,error

--- front/front/src/action/creatorHeroAction.js
+++ front/front/src/action/creatorHeroAction.js
@@ -1,16 +1,36 @@
 import ACTION_TYPES from './actionTypes';
 
-export const createHeroRequest = data =>({
+export const createHeroRequest = data => ({
   type: ACTION_TYPES.CREATE_HERO_REQUEST,
   data
 });
 
-export const createHeroSuccess = data =>({
+export const createHeroSuccess = data => ({
   type: ACTION_TYPES.CREATE_HERO_SUCCESS,
   data
 });
 
-export const createHeroError = error =>({
+export const createHeroError = error => ({
   type: ACTION_TYPES.CREATE_HERO_ERROR,
   error
+});
+
+export const getHeroesRequest = ({ limit, offset }) => ({
+  type: ACTION_TYPES.GET_HEROES_REQUEST,
+  payload: { limit, offset }
+});
+
+export const getHeroesSuccess = heroes => ({
+  type: ACTION_TYPES.GET_HEROES_SUCCESS,
+  payload: { heroes }
+});
+
+export const getHeroesError = ({ error }) => ({
+  type: ACTION_TYPES.GET_HEROES_ERROR,
+  payload: error
+});
+
+export const deleteHeroRequest = ({ id }) => ({
+  type: ACTION_TYPES.DELETE_HERO_REQUEST,
+  payload: { id }
 });
