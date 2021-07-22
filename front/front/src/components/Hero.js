9e100a7f2efa37d2c4ec72ccdbaa2a857fd763ab
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-22 11:20:24 +0300

    Add deleteHandler,deletHandler

--- front/front/src/components/Hero.js
+++ front/front/src/components/Hero.js
@@ -1,28 +1,45 @@
-import React,{useEffect} from 'react';
+import React, { useEffect,useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {deleteHeroRequest} from '../action/creatorHeroAction';
+import { deleteHeroRequest, isUpdateAction } from '../action/creatorHeroAction';
+import SuperPower from './SuperPower';
+import HeroUpdateFrom from './HeroUpdateFrom';
 
 const Hero = props => {
   const {
-    hero: { nickName, realName, discription, chartPhrase, id }
+    hero: { nickName, realName, originDescription, chartPhrase, id, SuperPowers },
+    hero
   } = props;
-
+  const [isUpdate, setIsUpdate]= useState(false)
   const dispatch = useDispatch();
 
-  const deleteHandler = ()=>{
-    dispatch(deleteHeroRequest({id}))
-  }
+  const deleteHandler = () => {
+    dispatch(deleteHeroRequest({ id }));
+  };
+  const updateHandler = () => {
+    setIsUpdate(prevIsUpdate=>prevIsUpdate=!isUpdate)
+  };
   
-
   return (
-    <>
-      <h1>{nickName}</h1>
-      <h2>{realName}</h2>
-      <p>{discription}</p>
-      <span>{chartPhrase}</span>
-      <button>update</button>
+    <li>
+       {isUpdate ? (
+        <HeroUpdateFrom hero={hero} updateHandler={updateHandler}/>
+      ) : (
+        <div>
+          <h1>{nickName}</h1>
+          <h2>{realName}</h2>
+          <p>{originDescription}</p>
+          <span>{chartPhrase}</span>
+          <ul>
+            {SuperPowers.map(power => (
+              <SuperPower key={power.id} power={power} idHero={id} />
+            ))}
+          </ul>
+        </div>
+      )} 
+      
+      <button onClick={updateHandler}>update</button>
       <button onClick={deleteHandler}>delete</button>
-    </>
+    </li>
   );
 };
 
