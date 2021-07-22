cb4b2e778dccc24e79afa120120208036d59df41
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-22 13:00:01 +0300

    Add createSuperPower

--- front/front/src/api/index.js
+++ front/front/src/api/index.js
@@ -16,24 +16,27 @@ export const getAllHeroes = async ({ limit = 5, offset = 0 }) => {
   );
   return responsePromise;
 };
-export const updateHero = async ({updateBody,id})=>{
-  const responsePromise = httpClient.patch(`/superhero/${id}`,updateBody)
+export const updateHero = async ({ updateBody, id }) => {
+  const responsePromise = httpClient.patch(`/superhero/${id}`, updateBody);
   return responsePromise;
-}; 
+};
 
 export const deleteHero = async ({ id }) => {
   const responsePromise = httpClient.delete(`/superhero/${id}`);
   return responsePromise;
 };
 
-
 export const deleteSuperPower = async ({ id, idHero }) => {
-  const responsePromise = httpClient.delete(`/superhero/${idHero}/superpower/${id}`);
+  const responsePromise = httpClient.delete(
+    `/superhero/${idHero}/superpower/${id}`
+  );
   return responsePromise;
 };
 
-export const createSuperPower = async ({id, createSuperPower})=>{
-  console.log(id);
-  const responsePromise = httpClient.post(`/superhero/${id}/superpower/`, createSuperPower);
+export const createSuperPower = async ({ id, createSuperPower }) => {
+  const responsePromise = httpClient.post(
+    `/superhero/${id}/superpower/`,
+    createSuperPower
+  );
   return responsePromise;
-}
+};
