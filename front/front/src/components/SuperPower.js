4c273b31e18d9a98f7964bf182b49d4d137453a1
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-07-22 11:18:44 +0300

    Add superPowerComponent

---
+++ front/front/src/components/SuperPower.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteSuperPowerRequest } from '../action/creatorHeroAction';
+
+
+const SuperPower = props => {
+  const {power, idHero}=props;
+  const dispatch = useDispatch();
+  const deleteHandler = ()=>{
+    dispatch(deleteSuperPowerRequest({idHero,id:power.id}))
+  }
+  return (
+    <li>
+      <h2>{power.name}</h2>
+      <span>{power.discription}</span>
+      <button onClick={deleteHandler}>Delete</button>
+    </li>
+  );
+}
+
+export default SuperPower;
